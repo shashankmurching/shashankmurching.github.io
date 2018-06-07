e23bd715c339d3ac48c59387ee5976cfd7cc35c4
Shashank Murching <shashankmurching@hotmail.com>
2018-06-06 21:54:41 -0400

    Set up foundation for website

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {Button} from 'react-md';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      &nbsp;
+        <h1 className="App-header">Hey! Thanks For Stopping By My Site! :)</h1>
+        <p className="App-intro">
+          This is a continuous work in progress.
+          But keep checking in for cool updates!
+        </p> 
+        <Button raised secondary>Mr. Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
