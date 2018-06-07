214c1f397f14b7566b20969eacd67e800227e110
Shashank Murching <shashankmurching@hotmail.com>
2018-06-07 18:01:12 -0400

    Messing with repo stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import {Welcome} from './Welcome.js'
 
 import {Button} from 'react-md';
 
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <div className="App">
       &nbsp;
+
         <h1 className="App-header">Hey! Thanks For Stopping By My Site! :)</h1>
         <p className="App-intro">
           This is a continuous work in progress.
---
+++ src/Welcome.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+
+// class Welcome extends React.Component {
+//   render () {
+//     return (
+//     	<h1> hi </h1>
+//     );
+//   }
+// }
