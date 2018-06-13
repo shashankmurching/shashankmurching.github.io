56f9cbef0abbfb918c77b0527fb5695b2f0c2d06
Shashank Murching <shashankmurching@hotmail.com>
2018-06-12 23:41:10 -0400

    updates

---
+++ src/SliderContent/ConnectWithMe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ConnectWithMe extends React.Component {
+  render () {
+    return (
+    	<div>
+    		<h1> Connect With Me! </h1>
+    		<h2> smurch@umich.edu </h2>
+			<div> [Add social media links / icons]
+
+			</div>
+
+	    </div>
+    );
+  }
+}
+
+export default ConnectWithMe;
---
+++ src/SliderContent/Resume.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Resume extends React.Component {
+  render () {
+    return (
+    	<div>
+	    	[link with google drive resume]
+	    	[Add clickable download option]
+	    </div>
+    );
+  }
+}
+
+export default Resume;
