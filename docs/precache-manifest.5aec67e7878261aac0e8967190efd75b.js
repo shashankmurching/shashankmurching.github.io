4dc01b3f1e9525a6f074a78750bb72e5df3aea3a
Shashank Murching <shashankmurching@hotmail.com>
2020-09-08 23:12:55 -0700

    Giving up and using docs instead

---
+++ docs/precache-manifest.5aec67e7878261aac0e8967190efd75b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8539e86534618592cdd4d9a0d3e81640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad0d4d35f83cc73376a7",
+    "url": "/static/css/main.09d8744f.chunk.css"
+  },
+  {
+    "revision": "34e8ab95c813b03a37f7",
+    "url": "/static/js/2.08573dad.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.08573dad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0d4d35f83cc73376a7",
+    "url": "/static/js/main.514daa43.chunk.js"
+  },
+  {
+    "revision": "8a47216ac6b1bc07b494",
+    "url": "/static/js/runtime-main.8cca3dff.js"
+  },
+  {
+    "revision": "bbf223e44f381ff62021e56864b6c473",
+    "url": "/static/media/Me.bbf223e4.jpg"
+  }
+]);
