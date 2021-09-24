7a6069620e4344af73901bcbed9d8e7ef5f780be
Shashank Murching <shashankmurching@hotmail.com>
2021-09-24 17:42:27 -0400

    Updating resume link

--- src/Const.js
+++ src/Const.js
@@ -1,6 +1,6 @@
 export const NavButtons = {
   Projects: 'https://github.com/shashankmurching',
-  Resume: 'https://drive.google.com/open?id=0BykdE61ta3-BRDN3dE4ydmpSQm8',
+  Resume: 'https://drive.google.com/file/d/0BykdE61ta3-BRDN3dE4ydmpSQm8/view?resourcekey=0-7--jCAewJfmz6oMNYx4Kmg',
   LinkedIn: 'https://www.linkedin.com/in/shashank-murching/',
   Email: 'mailto:smurch@umich.edu',
 };
